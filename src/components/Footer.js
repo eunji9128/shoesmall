f95aab0ee3d93863b25e97ad531275bc4f4052ec
eunji yang <eunji9128@gmail.com>
2023-04-25 18:45:09 +0900

    feat: gh-pages installed & footer content changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,24 @@ import styled from "styled-components";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
-            <p>footer blahblah</p>
-        </div>
+        <Box>
+            <h6>@SELEN(EUNJI), ALL RIGHTS RESERVED.</h6>
+            <p>서울특별시 광진구 자양동, eunji9128@gmail.com, https://github.com/eunji9128</p>
+        </Box>
     )
 };
+
+const Box = styled.div`
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+    background-color: #f8f9fa;
+    color: #888888;
+
+    h6, p {
+        margin: 4px;
+        font-size: 0.8em;
+    }
+`
 
 export default Footer
